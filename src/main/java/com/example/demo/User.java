d7e757acef480feddc0bf3d6b533c1d31805fbea
Bharat Kumar P <bharatkumarp28@gmail.com>
2022-01-21 20:24:12 +0530

    new changes

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+/*//@Controller
+*/
+@RestController
 public class User {
-    @ResponseBody
+	/* @ResponseBody */
     @RequestMapping("/")
     public String index() {
     	return "index";
