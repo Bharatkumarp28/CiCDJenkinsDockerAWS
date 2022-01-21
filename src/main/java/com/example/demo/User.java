72e6caeeafdea593c6dfb6f3fe76e6fc3f728385
Bharat Kumar P <bharatkumarp28@gmail.com>
2022-01-21 22:47:33 +0530

    HTML Page Done

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -2,17 +2,13 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-/*//@Controller
-*/
-@RestController
+@Controller
 public class User {
-	/* @ResponseBody */
+	
     @RequestMapping("/")
     public String index() {
-    	return "index";
+    	return "index.html";
     }
 
 }
